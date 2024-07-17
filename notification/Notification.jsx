e52786500bb2b585f1734af754f37ad48564d384
Promesse RUKUNDO <130702756+Promesse7@users.noreply.github.com>
2024-07-17 20:37:26 +0200

    Talkie files

---
+++ notification/Notification.jsx
@@ -0,0 +1,13 @@
+import { ToastContainer } from "react-toastfiy";
+import "react-toastfiy/dist/ReactToastify.css"
+import "./notification.css"
+
+const Notification = () =>{
+    return(
+        <div className="notification">
+
+        </div>
+    )
+}
+
+export default Notification
