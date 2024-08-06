cd3350ba05dbcb4f10a55d16ed4468db0135c65f
Promesse RUKUNDO <130702756+Promesse7@users.noreply.github.com>
2024-08-06 13:03:24 +0200

    chore: resolved register

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -7,7 +7,7 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import profile from "./rp.png";
 import Upload from "../lib/upload.js";
-import '../login/Login.css';
+import '../login/login.css';
 
 const Register = ({ onRegisterSuccess, onSwitchToLogin }) => {
     const [avatar, setAvatar] = useState({ file: null, url: "" });
