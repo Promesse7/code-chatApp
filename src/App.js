2c42da4ca62ce78b8fdd2966935a10e38ef8d903
Promesse RUKUNDO <130702756+Promesse7@users.noreply.github.com>
2024-08-06 11:59:25 +0200

    chore: resolve register import on app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import List from './components/list/list';
 import Chat from './components/chat/chat';
 import Detail from './components/details/detail';
 import Login from './components/login/Login';
-import Register from './components/register/Register'; // Import Register component
+import Register from './components/Register/Register'; // Import Register component
 import LandingPage from './components/landingPage/LandingPage';
 import Notification from './components/notification/Notification';
 import { useState, useEffect } from 'react';
