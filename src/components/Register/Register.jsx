4fe157dbada13927c15cd01b9ccda21ee172f416
Promesse RUKUNDO <130702756+Promesse7@users.noreply.github.com>
2024-08-06 18:05:32 +0200

    chore: updating styes

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -79,8 +79,9 @@ const Register = ({ onRegisterSuccess, onSwitchToLogin }) => {
                     <input type="email" placeholder="Email" name="email" required />
                     <input type="password" placeholder="Password" name="password" required />
                     <button disabled={loading}>{loading ? "Loading..." : "Sign Up"}</button>
+                    <button onClick={onSwitchToLogin}>Already have an account? Login here</button>
                 </form>
-                <button onClick={onSwitchToLogin}>Already have an account? Login here</button>
+                
             </div>
         </div>
     );
