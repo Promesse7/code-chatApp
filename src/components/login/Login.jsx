8d7bb99bd4982ca199819db434503455b7c5fffd
Promesse RUKUNDO <130702756+Promesse7@users.noreply.github.com>
2024-08-07 12:49:49 +0200

    chore: changing login page

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -50,6 +50,18 @@ const Login = ({ onLoginSuccess, onSwitchToRegister }) => {
                 </form>
                 
             </div>
+            <hr className="sep"/>
+            
+            <div className="attract">
+            <img src={logo} alt="" />
+            <div className="descri">
+                 <h1>Talkie your best chatting experience.</h1>
+            </div>
+            <div className="foot">
+                <span>PromCode</span>
+                <span>copyright 2024</span>
+            </div>
+            </div>
         </div>
     );
 };
