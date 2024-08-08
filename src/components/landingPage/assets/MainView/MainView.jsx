56e8a192c1a8e76b4f1df031bee7cb82d531c95e
Promesse RUKUNDO <130702756+Promesse7@users.noreply.github.com>
2024-08-08 15:52:08 +0200

    save

--- src/components/landingPage/assets/MainView/MainView.jsx
+++ src/components/landingPage/assets/MainView/MainView.jsx
@@ -67,10 +67,7 @@ const MainVew = ({ onGetStarted }) => {
                                         </div>
                               </div>
                         </div>
-                              <div className="foot">
-                                  <span>PromCode</span>
-                                  <span>Copyright 2024</span>
-                              </div>
+                              
         </div>
    
     )
